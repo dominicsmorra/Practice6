9631fdf43cc031ba25260336009f38cb49d00f81
Dominic Smorra <dominicsmorra@gmail.com>
2019-11-24 22:54:36 -0500

    Final Commit

--- exercise3.js
+++ exercise3.js
@@ -1,12 +1,16 @@
 function validate(){
+    //checks the username
     if(document.getElementById("user").value == ""){
         alert("Please enter your username");
     }
+    //checks the phone number
     if (document.getElementById("phone").value == "")
         alert("Please enter your phone");
+    //checks if there is a password
     if(document.getElementById("pass").value == ""){
         alert("Please enter your password");
     } 
+    //checks password length
     length = document.getElementById("pass").length
     if(length > 5 && length < 21){
         alert("Please enter a password of length between 6 and 20"); 
